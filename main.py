f174f6422b511a4fd23a25a126c077e4b93e494b
Glaccier <jules.graffan@gmail.com>
2025-12-10 09:12:00 +0100

    jn

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #  boucle principale -- > execution du programme complet
+from Pygame.pygame import Before_Game as BeGame 
+import pygame as pg
+def main():
+    """
+    Docstring for main
+    """
+    before_game = BeGame(pg.display.set_mode((400,400))) #400*400 n'est pas la taille définive
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == BeGame.Button_Left_click:
+
+    
