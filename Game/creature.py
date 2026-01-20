96e4e36808843bdc9337e41d30abe425a12a75ad
Sarah <Svignaudquantin09@gmail.com>
2026-01-20 15:22:42 +0100

    .

--- Game/creature.py
+++ Game/creature.py
@@ -134,9 +134,8 @@ class  Creature():
         False sinon. Toute la bouffe est stockée dans settings.food_list, c'est une liste d'objet (les coordonées sont dans le init )
         Les creatures on un champs de vision qui va de 1 a 10 donc plus c'est elevé plus elles voient loin
         """
-        if self.view >= Food.position() :
-            return True
-        return False
+        for food in settings.food_list:
+            pass
 
     def is_alive(self):
         """
